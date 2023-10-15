62711efc54de27729dc24c028bb5ab6bc970b7c7
jontyms <jontyms@jontyms.com>
2023-10-15 16:40:19 -0400

    greetd

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -20,7 +20,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
-
+    ./greetd.nix
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
   ];
