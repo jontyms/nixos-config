ab6acd02b101218d252762364acc747bab97523a
jontyms <jontyms@jontyms.com>
2023-10-15 18:54:28 -0400

    added polkit

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -115,6 +115,23 @@
   ];
   services.qemuGuest.enable = true;
   services.logind.enable = true;
+    security.polkit.extraConfig = ''
+polkit.addRule(function(action, subject) {
+      if (
+        subject.isInGroup("users")
+          && (
+            action.id == "org.freedesktop.login1.reboot" ||
+            action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+            action.id == "org.freedesktop.login1.power-off" ||
+            action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+          )
+        )
+      {
+        return polkit.Result.YES;
+      }
+    })
+  '';
+
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
 
