b61800d783d2fe50e6ffa9bb161230791709191f
jontyms <jontyms@jontyms.com>
2023-10-15 16:40:44 -0400

    removed sddm

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -119,6 +119,6 @@
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.enable = true;
+  #services.xserver.displayManager.sddm.enable = true;
+  #services.xserver.enable = true;
 }
